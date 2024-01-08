ccb2a0453dfcf5cbbdeb3cfb92812f72f19227ed
Joao Martins <milanezijoao51@gmail.com>
2024-01-08 18:10:58 -0300

    Criando o controlador LegalPerson

---
+++ Bank/Controllers/LPController.cs
@@ -0,0 +1,101 @@
+﻿using Bank.Context;
+using Bank.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LPController:ControllerBase
+{
+    private readonly AppDBContext _dbContext;
+
+    public LPController(AppDBContext context)
+    {
+        _dbContext = context;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<LegalPerson>> Get()
+    {
+        var persons = _dbContext.LegalPersons.ToList();
+        if (persons is null)
+        {
+            return NotFound("Não há contas");
+        }
+        return Ok(persons);
+    }
+    
+    [HttpGet("{id:int}", Name = "GetLegalPerson")]
+    public ActionResult<LegalPerson> Get(int id)
+    {
+        var personId = _dbContext.LegalPersons.Find(id);
+        if (personId is null)
+        {
+            return BadRequest("Conta não encontrada");
+        }
+        return Ok(personId);
+    }
+
+    [HttpPost]
+    public ActionResult Post(LegalPerson person)
+    {
+        if (person is null)
+        {
+            return BadRequest();
+        }
+        _dbContext.LegalPersons.Add(person);
+        _dbContext.SaveChanges();
+        return Ok(person);
+    }
+    [HttpPut("{id:int}")]
+    public ActionResult Put(LegalPerson person, int id)
+    {
+        if (person.Id != id)
+        {
+            return BadRequest("Id's diferentes");
+        }
+        var personId = _dbContext.LegalPersons.Find(id);
+        person.CNPJ = personId.CNPJ;
+        person.CorporateReason = personId.CorporateReason;
+        person.FantasyName = personId.FantasyName;
+        person.Number = personId.Number;
+        _dbContext.SaveChanges();
+        return Ok(personId);
+    }
+    [HttpDelete ("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var person = _dbContext.LegalPersons.Find(id);
+        if(person is null)
+        {
+            return BadRequest();
+        }
+        _dbContext.LegalPersons.Remove(person);
+        _dbContext.SaveChanges();
+        return NotFound();  
+    }
+    [HttpPut("{id:int}&{amount:double}", Name = "WihtdrawLegalPerson"), Tags("Withdraw")]
+    public ActionResult<LegalPerson> WihtDraw(int id, double amount)
+    {
+        var person = _dbContext.LegalPersons.Find(id);
+        if (person is null)
+        {
+            return BadRequest("Conta não encontrada");
+        }
+        person.Balance -= amount;
+        _dbContext.SaveChanges();
+        return Ok(person);
+    }
+    [HttpPut("{id:int}&&{amount:double}", Name = "DepositLegalPerson"), Tags("Deposit")]
+    public ActionResult<LegalPerson> Deposit(int id, double amount)
+    {
+        var person = _dbContext.LegalPersons.Find(id);
+        if (person is null)
+        {
+            return BadRequest("Conta não encontrada");
+        }
+        person.Balance += amount;
+        _dbContext.SaveChanges();
+        return Ok(person);
+    }
+}
+
--- Bank/Controllers/PPController.cs
+++ Bank/Controllers/PPController.cs
@@ -3,104 +3,105 @@ using Bank.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Bank.Controllers
+namespace Bank.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PPController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PPController:ControllerBase
+    private readonly AppDBContext _dbContext;
+
+    public PPController(AppDBContext context)
     {
-        private readonly AppDBContext _dbContext;
+        _dbContext = context;
+    }
 
-        public PPController(AppDBContext context)
+    [HttpGet]
+    public ActionResult<IEnumerable<PhysicalPerson>> Get()
+    {
+        var persons = _dbContext.PhysicalPersons.ToList();
+        if (persons is null)
         {
-            _dbContext = context;
+            return NotFound("Não há contas");
         }
+        return Ok(persons);
+    }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<PhysicalPerson>> Get()
+    [HttpGet("{id:int}", Name = "GetPhysicalPerson")]
+    public ActionResult<PhysicalPerson> Get(int id)
+    {
+        var personId = _dbContext.PhysicalPersons.Find(id);
+        if (personId is null)
         {
-            var persons=_dbContext.PhysicalPersons.ToList();
-            if(persons is null)
-            {
-                return NotFound("Não há contas");
-            }
-            return Ok(persons);
+            return BadRequest("Conta não encontrada");
         }
-       
-        [HttpGet("{id:int}", Name = "GetPhysicalPerson")]
-        public ActionResult<PhysicalPerson> Get(int id) 
-        {
-            var personId=_dbContext.PhysicalPersons.Find(id);
-            if(personId is null)
-            {
-                return BadRequest("Conta não encontrada");
-            }
-            return Ok(personId);
-        }
+        return Ok(personId);
+    }
 
-        [HttpPost]
-        public ActionResult Post(PhysicalPerson person)
+    [HttpPost]
+    public ActionResult Post(PhysicalPerson person)
+    {
+        if (person is null)
         {
-            if(person is null)
-            {
-                return BadRequest();
-            }
-            _dbContext.PhysicalPersons.Add(person);
-            _dbContext.SaveChanges();
-            return Ok(person);
+            return BadRequest();
         }
-        [HttpPut("{id:int}")]
-        public ActionResult Put(PhysicalPerson person,int id) 
+        _dbContext.PhysicalPersons.Add(person);
+        _dbContext.SaveChanges();
+        return Ok(person);
+    }
+    [HttpPut("{id:int}")]
+    public ActionResult Put(PhysicalPerson person, int id)
+    {
+        if (person.Id != id)
         {
-            if(person.Id != id)
-            {
-                return BadRequest("Id's diferentes");
-            }
-            var personId = _dbContext.PhysicalPersons.Find(id);
-            person.Name = personId.Name;
-            person.Birth=personId.Birth;
-            person.CPF=personId.CPF;
-            _dbContext.SaveChanges();
-            return Ok(personId);
+            return BadRequest("Id's diferentes");
         }
+        var personId = _dbContext.PhysicalPersons.Find(id);
+        person.Number = personId.Number;
+        person.Name = personId.Name;
+        person.Birth = personId.Birth;
+        person.CPF = personId.CPF;
+        _dbContext.SaveChanges();
+        return Ok(personId);
+    }
 
-        [HttpDelete ("{id:int}")]
-        public ActionResult Delete(int id)
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var personId = _dbContext.PhysicalPersons.Find(id);
+        if (personId is null)
         {
-            var personId = _dbContext.PhysicalPersons.Find(id);
-            if( personId is null)
-            {
-                return BadRequest("Conta não localizada");
-            }
-            _dbContext.PhysicalPersons.Remove(personId);
-            _dbContext.SaveChanges();
-            return NotFound();
+            return BadRequest("Conta não localizada");
         }
+        _dbContext.PhysicalPersons.Remove(personId);
+        _dbContext.SaveChanges();
+        return NotFound();
+    }
 
-        [HttpPut ("{id:int}&{amount:double}", Name = "Wihtdraw"),Tags("Withdraw")]
-        public ActionResult<PhysicalPerson> WihtDraw(int id,double amount)
+    [HttpPut("{id:int}&{amount:double}&{tax:double}", Name = "Wihtdraw"), Tags("Withdraw")]
+    public ActionResult<PhysicalPerson> WihtDraw(int id, double amount, double tax)
+    {
+        var person = _dbContext.PhysicalPersons.Find(id);
+        if (person is null)
         {
-            var person = _dbContext.PhysicalPersons.Find(id);
-            if(person is null)
-            {
-                return BadRequest("Conta não encontrada");
-            }
-            double totalAmount = amount + 5;
-            person.Balance-=totalAmount;
-            _dbContext.SaveChanges();
-            return Ok(person);
+            return BadRequest("Conta não encontrada");
         }
-        [HttpPut("{id:int}&&{amount:double}", Name = "Deposit"),Tags("Deposit")]
-        public ActionResult<PhysicalPerson> Deposit(int id, double amount)
+        double totalAmount = amount + tax;
+        person.Balance -= totalAmount;
+        _dbContext.SaveChanges();
+        return Ok(person);
+    }
+    [HttpPut("{id:int}&&{amount:double}", Name = "Deposit"), Tags("Deposit")]
+    public ActionResult<PhysicalPerson> Deposit(int id, double amount)
+    {
+        var person = _dbContext.PhysicalPersons.Find(id);
+        if (person is null)
         {
-            var person = _dbContext.PhysicalPersons.Find(id);
-            if (person is null)
-            {
-                return BadRequest("Conta não encontrada");
-            }
-            person.Balance += amount;
-            _dbContext.SaveChanges();
-            return Ok(person);
+            return BadRequest("Conta não encontrada");
         }
+        person.Balance += amount;
+        _dbContext.SaveChanges();
+        return Ok(person);
     }
 }
+
